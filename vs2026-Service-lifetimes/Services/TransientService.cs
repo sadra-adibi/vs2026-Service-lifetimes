22af74247dfd24b3807ef96453589dd5d52d0664
Sadra Adibi <sadraadibi@gmail.com>
2026-05-20 12:24:26 +0330

    adding TransientService class

---
+++ vs2026-Service-lifetimes/Services/TransientService.cs
@@ -0,0 +1,11 @@
+﻿namespace vs2026_Service_lifetimes.Services;
+
+public class TransientService
+{
+    private readonly Guid _id = Guid.NewGuid();
+
+    public Guid GetId()
+    {
+        return _id;
+    }
+}
